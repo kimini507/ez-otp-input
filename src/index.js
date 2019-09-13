34b5e07e199412deffb87d71022ef3641e9aeca6
Kim Joshua Advincula <kjcadvincula@gmail.com>
2019-09-13 16:56:10 +0800

    Added component to exports

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import EzOtpInput from './EzOtpInput.vue'
 export default {
   install(Vue, options) {
     Vue.component('ez-otp-input', EzOtpInput)
-  }
-}
+  },
 
+  // Components
+  EzOtpInput
+}
